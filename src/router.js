565a62ad09f361cdc03301c44a540f4395f4fb21
chereseeriepa <cherese.eriepa@outlook.com>
2019-12-09 21:28:00 +1300

    added skeleton about view

--- src/router.js
+++ src/router.js
@@ -2,14 +2,18 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Home from './views/Home.vue'
+import About from './views/About.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
-    { path: '/', redirect: '/home'},
-    { path: '/home', name: 'home', component: Home},
+    { path: '/', redirect: '/home' },
+    { path: '/home', name: 'home', component: Home },
+    { path: '/about', name: 'about', component: About },
     { path: '*', redirect: '/'}
+
+
   ]
 })
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="wrapper">
-    <h1> HOME </h1>
+    <h1> About </h1>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-
-    }
-  }
+  
 }
 </script>
